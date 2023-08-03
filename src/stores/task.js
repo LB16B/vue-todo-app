648e548beea910f0c950087d0715b26f857172dd
LB16B <ogawa.yui2000@gmail.com>
2023-08-04 07:53:43 +0900

    pinia

---
+++ src/stores/task.js
@@ -0,0 +1,28 @@
+import { defineStore } from "pinia";
+
+export const useTaskStore = defineStore('taskStore', {
+    // state データの読み書き
+    state: () => ({
+        tasks: [{
+            id: 1,
+            name: "First task",
+            is_completed: false
+        }, {
+            id: 2,
+            name: "Second task",
+            is_completed: true
+        }],
+        task: {
+            id: 1,
+            name: "First task",
+            is_completed: false
+        },
+    }),
+    // getters 加工したデータの読み書き
+    getters: {
+        completedTasks: (state) => state.tasks.filter(task => task.is_completed),
+        uncompletedTasks () {
+            return this.tasks.filter((task) => !task.is_completed)
+        }
+    },
+});
